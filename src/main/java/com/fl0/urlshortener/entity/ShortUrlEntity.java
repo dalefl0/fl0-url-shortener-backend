94fbc587abf18cdd0b203c9f946ac27863244c76
Dale Brett <138729757+dalefl0@users.noreply.github.com>
2023-07-25 00:27:27 +0530

    updated table name

--- src/main/java/com/fl0/urlshortener/entity/ShortUrlEntity.java
+++ src/main/java/com/fl0/urlshortener/entity/ShortUrlEntity.java
@@ -8,7 +8,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "short_urls1")
+@Table(name = "urls")
 public class ShortUrlEntity {
     @Id
     @Column(name = "id", nullable = false)
